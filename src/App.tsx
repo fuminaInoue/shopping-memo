a2d0f6601f830be10a3288db8e0e07452eea941a
fuminaInoue <fumina.mm@gmail.com>
2021-12-04 18:56:08 +0900

    設定ページへの遷移を作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { Memo } from 'components/pages/Memo'
+import { Setting } from 'components/pages/Setting'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Memo />} />
+        <Route path="/setting" element={<Setting />} />
       </Routes>
     </Router>
   )
