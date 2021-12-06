e077d0410a6fafd3cfcf56f97b5ef3486f0be056
fuminaInoue <fumina.mm@gmail.com>
2021-12-06 19:58:22 +0900

    タブにもテーマカラーを反映

--- src/components/molecules/Tab.tsx
+++ src/components/molecules/Tab.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { css } from '@emotion/react'
 
 type Props = {
@@ -9,6 +9,16 @@ type Props = {
 
 export const Tab: React.FC<Props> = ({ tabNumber, onCLickTab }) => {
   const tabs = ['tab1', 'tab2', 'tab3', 'tab4']
+  const [themeColor, setThemeColor] = useState('')
+
+  useEffect(() => {
+    if (localStorage.getItem('themeColor')) {
+      const theme = localStorage.getItem('themeColor')
+      setThemeColor(theme!)
+    } else {
+      setThemeColor('#afeeee')
+    }
+  }, [themeColor])
 
   return (
     <div css={tabWrapperStyle}>
@@ -16,6 +26,7 @@ export const Tab: React.FC<Props> = ({ tabNumber, onCLickTab }) => {
         return (
           <div
             key={v}
+            style={{ background: tabNumber === index ? themeColor : '#ccc' }}
             css={tabNumber === index ? activeTabStyle : tabStyle}
             onClick={() => onCLickTab(index)}
           >
@@ -34,21 +45,20 @@ const tabWrapperStyle = css({
 })
 
 const tabStyle = css({
-  border: '1px solid #ccc',
   padding: '4px 4px 2px 4px',
+  border: '1px solid #333',
   borderBottom: 'none',
   width: '25%',
   textAlign: 'center',
   fontWeight: 'bold',
   fontSize: 14,
   background: '#F2FCF9',
+  borderRadius: '8px 8px 0 0',
 })
 
 const activeTabStyle = css(tabStyle, {
   background: '#72C1DF',
-  color: '#fff',
   fontSize: 18,
   padding: '8px 8px 8px 8px',
   letterSpacing: 2,
-  borderRadius: '8px 8px 0 0',
 })
