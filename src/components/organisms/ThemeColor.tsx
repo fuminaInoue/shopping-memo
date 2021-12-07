c37662bd1cc09d22eda14e4815292068605c4e9f
fuminaInoue <fumina.mm@gmail.com>
2021-12-07 20:05:32 +0900

    テーマカラー設定をコンポーネントに切り出す

---
+++ src/components/organisms/ThemeColor.tsx
@@ -0,0 +1,53 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react'
+import { css } from '@emotion/react'
+
+type Props = {
+  setThemeColor: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const ThemeColor: React.FC<Props> = ({ setThemeColor }) => {
+  const onChangeThemeColor = (v: string) => {
+    localStorage.setItem('themeColor', v)
+    setThemeColor(v)
+  }
+
+  return (
+    <>
+      <div css={contentsStyle}>
+        <div css={themeStyle}>テーマカラー</div>
+        <select
+          css={themeSelectStyle}
+          name=""
+          id=""
+          onChange={(e) => onChangeThemeColor(e.target.value)}
+        >
+          <option value="#afeeee">ターコイズ</option>
+          <option value="#ffe4e1">ローズ</option>
+          <option value="#fffff0">アイボリー</option>
+          <option value="#9370db">むらさき</option>
+          <option value="#808080">グレー</option>
+        </select>
+      </div>
+    </>
+  )
+}
+
+const contentsStyle = css({
+  padding: '8px 0',
+  margin: '0 auto 8px',
+  width: '90%',
+  display: 'flex',
+  justifyContent: 'space-between',
+})
+
+const themeStyle = css({
+  fontWeight: 'bold',
+  fontSize: 18,
+})
+
+const themeSelectStyle = css({
+  fontSize: 18,
+  padding: '0 8px',
+  borderRadius: 5,
+})
--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import { css } from '@emotion/react'
 import { useNavigate } from 'react-router-dom'
+import { ThemeColor } from 'components/organisms/ThemeColor'
 import close from '../../images/close.svg'
 
 type Props = {}
@@ -9,10 +10,6 @@ type Props = {}
 export const Setting: React.FC<Props> = () => {
   const navigate = useNavigate()
   const [themeColor, setThemeColor] = useState('')
-  const onChangeThemeColor = (v: string) => {
-    localStorage.setItem('themeColor', v)
-    setThemeColor(v)
-  }
 
   useEffect(() => {
     if (localStorage.getItem('themeColor')) {
@@ -28,21 +25,7 @@ export const Setting: React.FC<Props> = () => {
       <div style={{ background: themeColor }} css={headerStyle}>
         <img src={close} alt="全件削除" onClick={() => navigate('/')} />
       </div>
-      <div css={contentsStyle}>
-        <div css={themeStyle}>テーマカラー</div>
-        <select
-          css={themeSelectStyle}
-          name=""
-          id=""
-          onChange={(e) => onChangeThemeColor(e.target.value)}
-        >
-          <option value="#afeeee">ターコイズ</option>
-          <option value="#ffe4e1">ローズ</option>
-          <option value="#fffff0">アイボリー</option>
-          <option value="#9370db">むらさき</option>
-          <option value="#808080">グレー</option>
-        </select>
-      </div>{' '}
+      <ThemeColor setThemeColor={setThemeColor} />
     </>
   )
 }
@@ -57,23 +40,4 @@ const headerStyle = css({
     width: 30,
     height: 30,
   },
-})
-
-const contentsStyle = css({
-  padding: '8px 0',
-  margin: '0 auto 8px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'space-between',
-})
-
-const themeStyle = css({
-  fontWeight: 'bold',
-  fontSize: 18,
-})
-
-const themeSelectStyle = css({
-  fontSize: 18,
-  padding: '0 8px',
-  borderRadius: 5,
 })
